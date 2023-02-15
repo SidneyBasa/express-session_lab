d5c0b33e4e97f1419c38a2585c9ee0bcf9719a08
Sidney Basa <basa206@hotmail.com>
2023-02-14 23:26:21 -0800

    Added additional protection to post route to create a chirp, new logout route

--- controllers/chirpController.js
+++ controllers/chirpController.js
@@ -32,24 +32,37 @@ router.get("/:id", (request, response)=>{
     })
 })
 
+// route to create a chirp
 router.post("/", (request, response)=>{
-    Chirp.create({
-        chirp:request.body.chirp,
+    
+    // Route protection shell
+    if(request.session.userId) {
+           Chirp.create({
+        
+            chirp:request.body.chirp,
+    
+            // Attaches the userId stored in sessions to the UserId of the chirp json data
+            UserId:request.session.userId
+    
+        }).then(chripData=>{
+            response.json(chripData)
+        })
+        .catch(error=>{
+            console.log("\x1B[33m----------------------------------------------")
+            // console.log("\x1B[36mPost request error:", error.errors[0].message);
+            console.log(error);
+            console.log("\x1B[33m----------------------------------------------\x1b[0m")
+            response.status(500).json({msg:"oh noes!", error})
+        })
 
-        // The UserId is an auto generated foreign key
-        // Created by the associations declared in the index.js at models
-        UserId:request.body.UserId
+    } else {
+        return response.status(403).json({msg:"You must login or sign up to create a chirp"})
+    }
+    
 
-    }).then(chripData=>{
-        response.json(chripData)
-    })
-    .catch(error=>{
-        console.log("\x1B[33m----------------------------------------------")
-        // console.log("\x1B[36mPost request error:", error.errors[0].message);
-        console.log(error);
-        console.log("\x1B[33m----------------------------------------------\x1b[0m")
-        response.status(500).json({msg:"oh noes!", error})
-    })
+
+
 })
+
 
 module.exports = router;
