888f5e1cd96374ceb114a0b659a13ba8fea29002
Sidney Basa <basa206@hotmail.com>
2023-02-16 23:21:29 -0800

    Displaying chrips to home handlebar page

--- controllers/frontEndController.js
+++ controllers/frontEndController.js
@@ -7,7 +7,36 @@ const {User, Chirp} = require('../models');
 // homepage
 // first route
 router.get("/", (request, response)=>{
-    response.render("home")
+    Chirp.findAll().then(chirpData=>{
+        console.log(chirpData)
+        // adding data to the homepage
+        // joeIsCool: true,
+        // cat:"asleep"
+        
+        // Taking the raw data
+        // And placing it in a form that handlebars can read
+        const handlebarChirps = chirpData.map(chirp=>chirp.toJSON())
+                // After the map method converts chirpData to json
+                // place it into the allChirps key to pass into views 
+                response.render("home",{
+                allChirps:handlebarChirps
+        })
+    })
+})
+
+// login route
+router.get("/login", (request, response)=>{
+    response.render("login")
+})
+
+// signup route
+router.get("/signup", (request, response)=>{
+    response.render("signup")
+})
+
+// profile route
+router.get("/profile", (request, response)=>{
+    response.render("profile")
 })
 
 module.exports = router;
